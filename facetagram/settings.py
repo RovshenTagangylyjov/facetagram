eb2cc3fabebec3e1c69661cb9f0e394edd05f079
Rovshen <tagangylyjowrowsen28@gmail.com>
2021-07-31 22:47:20 +0500

    deploymennt

--- facetagram/settings.py
+++ facetagram/settings.py
@@ -111,10 +111,10 @@ DATABASES = {
 #         'PORT': os.getenv('POSTGRES_PORT')
 #     }
 # }
- 
- import dj_database_url
- db_from_env = dj_database_url.config(conn_max_age=600)
- DATABASE['default'].update(db_from_env)
+
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=600)
+DATABASES['default'].update(db_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
