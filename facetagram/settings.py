9a40842cf928ccffb667728adab3d645e2c50e37
Rovshen <tagangylyjowrowsen28@gmail.com>
2021-07-31 21:40:27 +0500

    deployment

--- facetagram/settings.py
+++ facetagram/settings.py
@@ -148,6 +148,8 @@ STATICFILES_DIRS = [
 STATIC_ROOT = os.path.join(BASE_DIR, "assets")
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
