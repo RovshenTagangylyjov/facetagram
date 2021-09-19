11306e275e4c91c92989ad1dcad6e934c8c71631
rovshent <tagangylyjowrowsen28@gmail.com>
2021-09-19 21:30:46 +0500

    changed to postgres for local

--- facetagram/settings/base.py
+++ facetagram/settings/base.py
@@ -82,10 +82,14 @@ WSGI_APPLICATION = 'facetagram.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'facetagram',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'localhost',
     }
 }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
